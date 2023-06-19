79f923eed40c5eaeb1968e3a975d44a1161dfdef
Nicholas Lemes Bahia Lacerda <124929174+DevNicolasLacerda@users.noreply.github.com>
2023-06-18 23:19:12 -0300

    Add files via upload

---
+++ repo/config/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBYRXPyzlj3wLGBttIGyM1Rae4ycqLerAE",
+  authDomain: "eight-minds-c8b8b.firebaseapp.com",
+  projectId: "eight-minds-c8b8b",
+  storageBucket: "eight-minds-c8b8b.appspot.com",
+  messagingSenderId: "970355764676",
+  appId: "1:970355764676:web:38b75368c987e86ade4891"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
